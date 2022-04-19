3d56975f29795671e5613d849fb09ac45ab482e6
Unknown <rodrigo.rocha.103@A305-13L.imd.local>
2022-04-19 14:29:39 -0300

    add sqlite connection example

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,6 @@ router.get('/', function(req, res, next) {
   res.send({ hello: 'world' });
 });
 
+
+
 module.exports = router;
--- src/routes/tasksRoute.js
+++ src/routes/tasksRoute.js
@@ -5,4 +5,6 @@ const TasksController = require('../controllers/tasksController')
 router.get('/', TasksController.list);
 router.get('/:id', TasksController.get)
 
+
+
 module.exports = router;
