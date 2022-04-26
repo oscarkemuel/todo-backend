e45cd626ecd3ab564add00362e30c39558772c2c
Oscar Kemuel <oscar.kemuel5@gmail.com>
2022-04-26 14:42:22 -0300

    feat: configure endpoints existents

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -1,9 +1,30 @@
+var db = require("../database/database.js")
+
 exports.list = (req, res) => {
-  res.status(200).json([{ name: 'task 1'}, {name: 'task 2'}, {name: 'task 3'}])
+  const sql = "select * from task"
+  const params = []
+
+  db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({erro: { message: "Erro na listagem" }});
+        return;
+      }
+      res.status(200).json(rows);
+    });
+
+  // res.status(200).json([{ name: 'task 1'}, {name: 'task 2'}, {name: 'task 3'}])
 }
 
 exports.get = (req, res) => {
   const id = req.params.id;
+  const sql = "SELECT * FROM task WHERE id = ?";
+  const params = [req.params.id]
 
-  res.status(200).json({name: `task ${id}`})
+  db.all(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({erro: { message: "Erro ao buscar" }});
+      return;
+    }
+    res.status(200).json(row);
+  });
 }
